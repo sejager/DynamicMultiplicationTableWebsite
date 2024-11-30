0069f464ca5bbad1c5b81cf757f2c2f0e2e22185
Sebi <167681990+sejager@users.noreply.github.com>
2024-11-30 04:00:37 -0500

    Start of the implementation of tabs. Also separate the functions for better visibility.

--- multtable.js
+++ multtable.js
@@ -5,11 +5,23 @@ This is a JavaScript file to make the dynamic multiplication table dynamic.
 */
 
 
-// Thanks to https://jqueryvalidation.org for helping with jQuery validation
-
-// Once the document is ready create the needed error methods and assign them to the relevant elements.
+// Have the needed functions be run once the document has loaded.
 $(document).ready(function() {
-    // https://stackoverflow.com/a/29451695 how to compare values
+    validator();
+    sliders();
+    tabs();
+
+    // Create a new tab with a table if the form is valid upon clicking of submit
+    $('#subbtn').click(function () {
+        if ($('#numValues').valid() == true)
+            tableCreate();
+    });
+});
+
+function validator() {
+    // Thanks to https://jqueryvalidation.org for helping with jQuery validation
+    // Create the needed error methods and assign them to the relevant elements.
+    // Thanks to https://stackoverflow.com/a/29451695 for how to compare values
     jQuery.validator.addMethod('greaterThan', function (value, element, param) {
         return parseInt(value) > parseInt($(param).val());
     }, jQuery.validator.format('The minimum value must be lower than the maximum value.'));
@@ -45,6 +57,9 @@ $(document).ready(function() {
         }
     })
 
+}
+
+function sliders() {
     // Slider settings
     // Thanks to https://jesseheines.com/~heines/91.461/91.461-2015-16f/461-assn/jQueryUI1.8_Ch06_SliderWidget.pdf
     // The slide function updates the numeric value in the input field as the slider moves
@@ -114,79 +129,84 @@ $(document).ready(function() {
     setInterval(function() {$('#maxrow').each(function() {
         $('#maxrowslider').slider('value', $('#maxrow').val())
     })}, 1)
-});
-
-// Thanks to various w3schools pages for helping.
-function tableCreate() {
-    // Get the values from the submission form to make a table
-    var mincol = document.getElementById("mincol").value;
-    var maxcol = document.getElementById("maxcol").value;
-    var minrow = document.getElementById("minrow").value;
-    var maxrow = document.getElementById("maxrow").value;
-
-    // Get rid of the old table if necessary.
-    // Thanks to https://stackoverflow.com/a/32777600
-    if (document.getElementById("tableContainer")) {
-        var oldContainer = document.getElementById("tableContainer");
-        oldContainer.parentElement.removeChild(oldContainer);
-    }
-
-    // Create a new table container with all its contents
-
-    const tblCntnr = document.createElement("div");
-    tblCntnr.setAttribute("id", "tableContainer");
-
-    // Create a new table
-    const tbl = document.createElement("table");
-    tbl.setAttribute("id", "tbl");
-    tbl.setAttribute("overflow", "scroll");
-
-    const tblBody = document.createElement("tbody");
-
-    var row, cell, cellNo;
-    
-    /* Create the table cell by cell, column by column, row by row by
-    creating elements for each cell and then inserting text nodes within them
-    and then inserting the cell into the row. Next insert the row into the
-    table body until it is complete before placing the table body within the
-    table. */
-    // Thanks to https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
-    for (let i = minrow - 1; i <= maxrow; i++) {
-        row = document.createElement("tr");
-        for (let j = mincol - 1; j <= maxcol; j++) {
-            // Exceptions for header rows and columns
-            if (i == minrow - 1) {
-                cell = document.createElement("th");
-                if (j == mincol - 1) {
-                    cellNo = document.createTextNode("");
-                } else {
-                    cellNo = document.createTextNode(j)
-                }
-            } else if (j == mincol - 1) {
-                cell = document.createElement("th");
-                cellNo = document.createTextNode(i)
-            } else {
-                cell = document.createElement("td");
-                cellNo = document.createTextNode(i * j);
-            }
-            cell.appendChild(cellNo);
-            row.appendChild(cell);
-        }
-    
-        tblBody.appendChild(row);
-    }
-    tbl.appendChild(tblBody);
-
-    // Attach the newly created table onto the document
-    document.body.appendChild(tblCntnr);
-    document.getElementById("tableContainer").appendChild(tbl);
 }
 
-// Have the submit button be the trigger to create the table, but first check that the form is valid
-// Thanks to https://stackoverflow.com/a/69773796
-var btn = document.getElementById("subbtn");
-btn.addEventListener("click", () => {
-    if ($('#numValues').valid() == true) {
-        tableCreate();
-    }
-})
+function tabs() {
+    // Tab settings
+    // Thanks to https://jesseheines.com/~heines/91.461/91.461-2015-16f/461-assn/jQueryUI1.8_Ch03_TabsWidget.pdf
+    $('#tabs').tabs();
+}
+
+function tableCreate() {
+        // Get the latest values
+        var mincol = $('#mincol').val()
+        var maxcol = $('#maxcol').val()
+        var minrow = $('#minrow').val()
+        var maxrow = $('#maxrow').val()
+        // Create a tab name and ID based on the values
+        var tabName = 'col: ' + mincol + ' ' + maxcol + '<br>'
+            + ' row: ' + minrow + ' ' + maxrow;
+        var tabId = mincol + maxcol + minrow + maxrow;
+
+        // Don't create a new tab if we already have a tab with those values
+        if (document.getElementById(tabId) != null) {
+            console.log('Tab already exists.');
+            return;
+        }
+
+        $('#tabs').find('ul').append('<li><a href="' + tabId + '">' + '<div>'
+            + tabName + '</div>' + '</a></li>');
+
+        // Create a new table container with all its contents
+        const tblCntnr = document.createElement("div");
+        tblCntnr.setAttribute("id", "tableContainer");
+
+        // Create a new table
+        const tbl = document.createElement("table");
+        tbl.setAttribute("id", "tbl");
+        tbl.setAttribute("overflow", "scroll");
+
+        const tblBody = document.createElement("tbody");
+
+        var row, cell, cellNo;
+        
+        /* Create the table cell by cell, column by column, row by row by
+        creating elements for each cell and then inserting text nodes within them
+        and then inserting the cell into the row. Next insert the row into the
+        table body until it is complete before placing the table body within the
+        table. */
+        // Thanks to https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+        for (let i = minrow - 1; i <= maxrow; i++) {
+            row = document.createElement("tr");
+            for (let j = mincol - 1; j <= maxcol; j++) {
+                // Exceptions for header rows and columns
+                if (i == minrow - 1) {
+                    cell = document.createElement("th");
+                    if (j == mincol - 1) {
+                        cellNo = document.createTextNode("");
+                    } else {
+                        cellNo = document.createTextNode(j)
+                    }
+                } else if (j == mincol - 1) {
+                    cell = document.createElement("th");
+                    cellNo = document.createTextNode(i)
+                } else {
+                    cell = document.createElement("td");
+                    cellNo = document.createTextNode(i * j);
+                }
+                cell.appendChild(cellNo);
+                row.appendChild(cell);
+            }
+        
+            tblBody.appendChild(row);
+        }
+        tbl.appendChild(tblBody);
+        tblCntnr.appendChild(tbl);
+
+        // Append the newly created table to the new tab
+        // Thanks to https://stackoverflow.com/a/18076415 and https://stackoverflow.com/a/21515395
+        $('#tabs').append('<div id="' + tabId + '">hey</div>')
+        $('#tabs').append(tblCntnr);
+        $('#tabs').append('</div>');
+        $('#tabs').tabs('refresh');
+};
